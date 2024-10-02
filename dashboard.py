21ac28b18853144981b626609e4805cf994d87d1
AndiftiUKDW <andi.putrayana@ti.ukdw.ac.id>
2024-10-02 13:54:48 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -34,76 +34,22 @@ def create_state(df):
     merged = Brazil.merge(Brazil_data_df, left_on='state_code', right_on='state_code')
     return merged
 
-def create_rfm(df,end):
+def create_rfm(df):
     rfm_df = df.groupby(by='customer_id', as_index=False).agg({
         "order_approved_at":"max",
         "order_id":"nunique",
         "price": "sum"
     })
+    end = rfm_df['order_approved_at'].max()
     rfm_df["recentcy"] = rfm_df['order_approved_at'].apply(lambda x: (end-x).days)
+    rfm_df = rfm_df.sort_values(by="recentcy",ascending=True)
     rfm_df["index"] = range(1, len(rfm_df)+1)
     rfm_df.drop("order_approved_at",axis=1,inplace=True)
     rfm_df.columns = ["customer_id","frequency","monetary","recentcy" ,"index"]
     return rfm_df
 
 
-
-customers_df = pd.read_csv("./Dataset/customers_dataset.csv")
-order_items_df = pd.read_csv("./Dataset/order_items_dataset.csv")
-order_payments_df = pd.read_csv("./Dataset/order_payments_dataset.csv")
-order_reviews_df = pd.read_csv("./Dataset/order_reviews_dataset.csv")
-orders_df = pd.read_csv("./Dataset/orders_dataset.csv")
-products_df = pd.read_csv("./Dataset/products_dataset.csv")
-translate_df = pd.read_csv("./Dataset/product_category_name_translation.csv")
-sellers_df = pd.read_csv("./Dataset/sellers_dataset.csv")
-order_items_df["shipping_limit_date"] = pd.to_datetime(order_items_df["shipping_limit_date"])
-kolom = ["order_approved_at","order_purchase_timestamp","order_delivered_carrier_date","order_delivered_customer_date","order_estimated_delivery_date"]
-for isi in kolom:
-    orders_df[isi] = pd.to_datetime(orders_df[isi])
-product_translated_df = pd.merge(
-    left = products_df,
-    right = translate_df,
-    how = "left",
-    left_on = "product_category_name",
-    right_on = "product_category_name"
-)
-all_order_df = pd.merge(
-    left=orders_df,
-    right=order_payments_df,
-    how="left",
-    left_on="order_id",
-    right_on="order_id",
-)
-all_order_df = pd.merge(
-    left=all_order_df,
-    right=order_items_df,
-    how="left",
-    left_on="order_id",
-    right_on="order_id",
-)
-all_order_df = pd.merge(
-    left=all_order_df,
-    right=order_reviews_df,
-    how="left",
-    left_on="order_id",
-    right_on="order_id",
-)
-order_product_df = pd.merge(
-    left=all_order_df,
-    right=product_translated_df,
-    how="left",
-    left_on="product_id",
-    right_on="product_id",
-)
-all_df = pd.merge(left=order_product_df,
-    right=customers_df,
-    how="left",
-    left_on="customer_id",
-    right_on="customer_id",
-    )
-
-
-
+all_df = pd.read_csv("all_df.csv")
 columns = ["order_purchase_timestamp","order_approved_at","order_delivered_carrier_date","order_delivered_customer_date","order_estimated_delivery_date","shipping_limit_date"]
 for column in columns:
     all_df[column] = pd.to_datetime(all_df[column])
@@ -117,12 +63,12 @@ with st.sidebar:
         label='Time Frame', min_value=min_date, max_value=max_date,
         value=[min_date,max_date]
     )
-
-main_df = all_df[(all_df["order_approved_at"] >= str(start_date))&(all_df["order_approved_at"]<= str(end_date))]
+st.write(str(end_date)+" 23:00:00")
+main_df = all_df[(all_df["order_approved_at"] >= str(start_date))&(all_df["order_approved_at"]<= str(end_date)+" 23:00:00")]
 bulanan_df = create_order_bulanan(main_df)
 category_df = create_category(main_df)
 brazil_df = create_state(main_df)
-rfm_df = create_rfm(main_df,pd.to_datetime(end_date))
+rfm_df = create_rfm(main_df)
 
 st.header("Brazil E-Commerce (Olist Store) Dashboard")
 st.subheader("Monthly Order")
@@ -221,3 +167,4 @@ ax[2].set_title("By Spending", loc="center", fontsize=50)
 ax[2].tick_params(axis ='x', labelsize=25)
 ax[2].tick_params(axis='y', labelsize=30)
 st.pyplot(fig)
+st.dataframe(rfm_df)
